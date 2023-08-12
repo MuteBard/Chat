72a7412af15475a84c7c0da92903450c9b532580
Mutebard <csevere@brandeis.edu>
2023-08-12 09:58:59 -0400

    clean up folder name and imports

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const openAI = require("./openAPI/chat");
+const openAI = require("./openAI/chat");
 const settings = require("./settings");
 const session = require("express-session");
 
